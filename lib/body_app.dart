1e7c41a4398f188819b280a9d338bb9de03b50fb
RamonOliveira95 <ramonsilvadeoliveira95@hotmail.com>
2022-10-03 17:05:45 -0300

    first commit

---
+++ lib/body_app.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class BodyApp extends StatefulWidget {
+  const BodyApp({Key? key}) : super(key: key);
+
+  @override
+  State<BodyApp> createState() => _BodyAppState();
+}
+
+class _BodyAppState extends State<BodyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      body: Center(
+        child: SizedBox(
+          width: 280,
+          height: double.infinity,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Calculadora IMC",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40),
+              Row(
+                children: const [
+                  Text(
+                    "Peso (Kg)",
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  ),
+                ],
+              ),
+              TextField(),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: const [
+                  Text(
+                    "Altura (cm)",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              TextField(),
+              const SizedBox(height: 40),
+              TextButton(
+                onPressed: () {},
+                child: Container(
+                  height: 40,
+                  width: 280,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(40.0),
+                  ),
+                  child: Container(
+                    width: 30,
+                    height: 40,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Calcular",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'body_app.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[850],
+        title: SizedBox(
+          width: double.infinity,
+          height: 40,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: 30,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40.0)
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                  ),
+                  child: const Text(
+                    "Resetar",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: BodyApp(),
+    );
+  }
+}
